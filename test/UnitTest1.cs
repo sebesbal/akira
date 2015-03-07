e269f95ab4927ea3b0cb94e0b4ed02d8d1ff96c1
sebesbal <sebesbal@gmail.com>
2015-03-08 00:05:23 +0100

    Match

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using akira;
+using System.Xml.Linq;
+using System.Linq;
 
 namespace test
 {
@@ -20,6 +22,32 @@ namespace test
             Akira a = new Akira();
             a.Run("test_parser.aki");
             a.Save("result.xml");
+        }
+
+        [TestMethod]
+        public void TestMatch()
+        {
+            Akira a = new Akira();
+            a.Run("test_match.aki");
+            a.Save("result.xml");
+        }
+
+        [TestMethod]
+        public void ShouldGetXElementDeepCopyUsingConstructorArgument()
+        {
+            XElement original = new XElement("original");
+            XElement orichild = new XElement("orichild");
+            original.Add(orichild);
+
+            XElement deepCopy = new XElement(original);
+            // XElement newchild = new XElement("orichild");
+            //deepCopy.Add(newchild);
+
+            var newchild = deepCopy.Elements().First();
+            newchild.Name = "lofusz";
+                //. = "copy";
+            // Assert.AreEqual("original", original.Name);
+            Assert.AreEqual("orichild", orichild.Name);
         }
     }
 }
