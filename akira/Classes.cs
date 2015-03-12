c7a4b05caa7e6e511cb908ca8db6ae95743d24ec
sebesbal <sebesbal@gmail.com>
2015-03-13 00:49:00 +0100

    fos

--- akira/Classes.cs
+++ akira/Classes.cs
@@ -121,6 +121,31 @@ namespace akira
             }
             return false;
         }
-    }
 
+        public static Assembly AssemblyFromCode(string code)
+        {
+            var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } });
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll", "System.Xml.dll", "System.Xml.Linq.dll", "akira.dll", "match.dll"}, "gen0.dll", true);
+            parameters.GenerateExecutable = false;
+            
+            CompilerResults results = csc.CompileAssemblyFromSource(parameters, code);
+            results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
+            var assembly = results.CompiledAssembly;
+            // results.
+            return assembly;
+        }
+
+        public static object InstanceFromCode(string code)
+        {
+            var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll", "System.Xml.dll", "System.Xml.Linq.dll", "akira.dll" });
+            parameters.GenerateExecutable = false;
+            CompilerResults results = csc.CompileAssemblyFromSource(parameters, code);
+            results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
+            var assembly = results.CompiledAssembly;
+
+            var t = assembly.GetTypes()[0];
+            return assembly.CreateInstance(t.FullName);
+        }
+    }
 }
--- match/Match.cs
+++ match/Match.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -20,25 +22,34 @@ namespace akira
 
             if (!(a == null || a.Value == "match")) return false;
 
-            Instance result = new Instance(node.Elements().ElementAt(0), node.Elements().ElementAt(1));
+            // Instance result = new Instance(node.Elements().ElementAt(0), node.Elements().ElementAt(1));
+            Instance result = GenerateInstance(ctx, node);
             ctx.Rules.Add(node.Attribute("src").Value, result);
 
             node.Remove();
             node = null;
 
+            //XElement m = new XElement("txt", GenerateClass(ctx, node));
+            //node.ReplaceWith(m);
+            //node = m;
+
             return true;
         }
 
-        void FindRefs(XElement node, List<string> refs)
+        void FindRefs(XElement node, List<string> refs, List<string> conds)
         {
             XAttribute a;
             if (node.GetAttribute("ref", out a))
             {
                 refs.Add(a.Value);
             }
+            if (node.Name == "cond")
+            {
+                conds.Add(node.Value);
+            }
             foreach (var item in node.Elements())
             {
-                FindRefs(item, refs);
+                FindRefs(item, refs, conds);
             }
         }
 
@@ -46,144 +57,202 @@ namespace akira
 
         protected string GenerateClass(Context ctx, XElement node)
         {
-            
             List<string> refs = new List<string>();
-            FindRefs(node, refs);
+            List<string> conds = new List<string>();
+            FindRefs(node.Elements().ElementAt(0), refs, conds);
             string refDefs = "";
             foreach (var item in refs)
             {
-                refDefs += "public XElement " + item + ";\n";
+                refDefs += "[IsVar()] public XElement " + item + "; ";
             }
 
-            string code = "public class " + ctx.GenName() + " : akira.Match.Instance"
-                + "{\n"
-                + refDefs
-                + "}\n";
+            string condDefs = "";
+            foreach (var item in conds)
+            {
+                condDefs += "conds.Add(delegate(){ " + item + "}); ";
+            }
+
+            string name = ctx.GenName();
+            string code = "using System; using akira; using System.Xml.Linq;"
+            + "namespace akira {"
+            + "public class " + name + " : akira.Instance"
+            + "{ "
+            // + refDefs
+            + "public " + name + "(){ " + condDefs + "}"
+            + "}} ";
 
             return code;
         }
 
-        [System.AttributeUsage(System.AttributeTargets.Field)]
-        public class IsVar : System.Attribute
+        //public class gen0 : akira.Match.Instance
+        //{
+
+        //}
+
+        protected Instance GenerateInstance(Context ctx, XElement node)
         {
-            public IsVar() { }
+            string code = GenerateClass(ctx, node);
+
+            // var a = Akira.AssemblyFromCode(code);
+
+            Assembly assembly = Assembly.LoadFile(Directory.GetCurrentDirectory() + "/gen0.dll");
+
+            Type t = assembly.GetTypes()[0];
+            if (t.IsSubclassOf(typeof(Instance)))
+            {
+                var o = assembly.CreateInstance(t.FullName);
+                Instance i = o as Instance;
+                i.left = node.Elements().ElementAt(0);
+                i.right = node.Elements().ElementAt(1);
+                return i;
+            }
+            return null;
+            
+        }
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class IsVar : System.Attribute
+    {
+        public IsVar() { }
+    }
+
+    public delegate bool Condition();
+
+    public class Instance : Rule
+    {
+        public XElement left;
+        public XElement right;
+
+        //[IsVar()]
+        //public XElement a;
+        //[IsVar()]
+        //public XElement b;
+
+        protected List<Condition> conds = new List<Condition>();
+
+        int condIndex = 0;
+
+        //public Instance(XElement left, XElement right)
+        //{
+        //    this.left = left;
+        //    this.right = right;
+
+        //    //conds.Add(delegate()
+        //    //{
+        //    //    return a.Name == "num";
+        //    //});
+        //}
+        public override bool Apply(Context ctx, ref XElement node)
+        {
+            clearVars();
+
+            if (unify(left, node))
+            {
+                var result = construct(right);
+                node.ReplaceWith(result);
+                node = result;
+                return true;
+            }
+
+            return false;
         }
 
-        public class Instance: Rule
+        bool unify(XElement a, XElement b)
         {
-            XElement left;
-            XElement right;
+            return false;
 
-            [IsVar()]
-            public XElement a;
-            [IsVar()]
-            public XElement b;
-
-            public Instance(XElement left, XElement right)
+            if (a.Name == "any")
             {
-                this.left = left;
-                this.right = right;
-            }
-            public override bool Apply(Context ctx, ref XElement node)
-            {
-                clearVars();
-
-                if (unify(left, node))
+                string av = a.Attribute("ref").Value;
+                if (varDefined(av))
                 {
-                    var result = construct(right);
-                    node.ReplaceWith(result);
-                    node = result;
-                    return true;
-                }
-
-                return false;
-            }
-
-            bool unify(XElement a, XElement b)
-            {
-                if (a.Name == "any")
-                {
-                    string av = a.Attribute("ref").Value;
-                    if (varDefined(av))
-                    {
-                        return unify(getVar(av), b);
-                    }
-                    else
-                    {
-                        setVar(av, b);
-                        return true;
-                    }
-                }
-                else if (a.Name == b.Name && a.Nodes().Count() == b.Nodes().Count())
-                {
-                    var bitem = b.Elements().GetEnumerator();
-                    bitem.MoveNext();
-                    foreach (var aitem in a.Elements())
-                    {
-                        if (!unify(aitem, bitem.Current))
-                        {
-                            return false;
-                        }
-                        bitem.MoveNext();
-                    }
-                    return true;
-                }
-
-                return false;
-            }
-
-            void setVar(string name, XElement node)
-            {
-                GetType().GetField(name).SetValue(this, node);
-            }
-
-            XElement getVar(string name)
-            {
-                return (XElement)typeof(Instance).GetField(name).GetValue(this);
-            }
-
-            bool varDefined(string name)
-            {
-                return getVar(name) != null;
-            }
-
-            bool isVar(System.Reflection.FieldInfo field)
-            {
-                return field.GetCustomAttributes(typeof(IsVar), false).Count() > 0;
-            }
-
-            void clearVars()
-            {
-                foreach (var item in GetType().GetFields())
-                {
-                    if (isVar(item))
-                    {
-                        item.SetValue(this, null);
-                    }
-                }
-            }
-
-            XElement construct(XElement template)
-            {
-                template = template.DeepCopy();
-                ReplaceRefs(template);
-                return template;
-            }
-
-            void ReplaceRefs(XElement elem)
-            {
-                if (elem.Name == "ref")
-                {
-                    // elem.ReplaceWith(map[elem.Value].DeepCopy());
-                    elem.ReplaceWith(getVar(elem.Value).DeepCopy());
+                    if (!unify(getVar(av), b)) return false;
                 }
                 else
                 {
-                    var v = elem.Elements().ToArray();
-                    foreach (var item in v)
+                    setVar(av, b);
+                }
+            }
+
+            if (a.Name == "any" || a.Name == b.Name) // && a.Nodes().Count() == b.Nodes().Count())
+            {
+                var bitem = b.Elements().GetEnumerator();
+                bitem.MoveNext();
+                foreach (var aitem in a.Elements())
+                {
+                    if (aitem.Name == "cond")
                     {
-                        ReplaceRefs(item);
+                        var cond = conds[condIndex++];
+                        if (!cond())
+                        {
+                            return false;
+                        }
                     }
+                    else if (!unify(aitem, bitem.Current))
+                    {
+                        return false;
+                    }
+                    bitem.MoveNext();
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        void setVar(string name, XElement node)
+        {
+            GetType().GetField(name).SetValue(this, node);
+        }
+
+        XElement getVar(string name)
+        {
+            return (XElement)typeof(Instance).GetField(name).GetValue(this);
+        }
+
+        bool varDefined(string name)
+        {
+            return getVar(name) != null;
+        }
+
+        bool isVar(System.Reflection.FieldInfo field)
+        {
+            return field.GetCustomAttributes(typeof(IsVar), false).Count() > 0;
+        }
+
+        void clearVars()
+        {
+            condIndex = 0;
+            foreach (var item in GetType().GetFields())
+            {
+                if (isVar(item))
+                {
+                    item.SetValue(this, null);
+                }
+            }
+        }
+
+        XElement construct(XElement template)
+        {
+            template = template.DeepCopy();
+            ReplaceRefs(template);
+            return template;
+        }
+
+        void ReplaceRefs(XElement elem)
+        {
+            if (elem.Name == "ref")
+            {
+                // elem.ReplaceWith(map[elem.Value].DeepCopy());
+                elem.ReplaceWith(getVar(elem.Value).DeepCopy());
+            }
+            else
+            {
+                var v = elem.Elements().ToArray();
+                foreach (var item in v)
+                {
+                    ReplaceRefs(item);
                 }
             }
         }
